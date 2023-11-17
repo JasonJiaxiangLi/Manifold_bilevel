82f94fc6904f95d7cc4bbda72fb9200cc232512b
JasonJiaxiangLi <jasonljx96@gmail.com>
2023-11-16 17:24:26 -0800

    verify gradient of karcher mean

--- function_derivative_tests/functions_tr_logm.py
+++ function_derivative_tests/functions_tr_logm.py
@@ -5,72 +5,69 @@ by www.matrixcalculus.org
 
 from input
 
-d/dA tr(B'*log(A)) = B'*inv(A)
+d/dA tr(C * log(P X Q))
 
 where
 
-A is a symmetric psd matrix
-B is a symmetric psd matrix
-
-The generated code is provided "as is" without warranty of any kind.
+X is a symmetric psd matrix
+P, Q, C are cosntant symmetric psd matrix
 """
 
 from __future__ import division, print_function, absolute_import
 
 import numpy as np
-
 import scipy
 
-def fAndG(A, B):
-    assert isinstance(A, np.ndarray)
-    dim = A.shape
-    assert len(dim) == 2
-    A_rows = dim[0]
-    A_cols = dim[1]
-    assert isinstance(B, np.ndarray)
-    dim = B.shape
-    assert len(dim) == 2
-    B_rows = dim[0]
-    B_cols = dim[1]
-    assert B_rows == A_cols
-    assert B_cols == A_rows
+def symm(A):
+    return (A + A.T) / 2
 
-    functionValue = np.trace(B.dot(scipy.linalg.logm(A)))
-    invA = np.linalg.inv(A)
-    sqrt_invA = scipy.linalg.sqrtm(invA)
-    gradient = B.dot(invA)
+def fAndG(X, P, Q, C):
+    functionValue = np.trace(C.dot(P.dot(X).dot(Q)))
+    d = X.shape[0]
+    gradient = np.zeros_like(X)
+    zeros = np.zeros_like(X)
+    temp1 = np.block([[zeros, C], [zeros, zeros]])
+    temp2 = np.block([[P, zeros], [zeros, P]])
+    temp4 = np.block([[Q, zeros], [zeros, Q]])
+    for i in range(d):
+        for j in range(d):
+            Eij = np.zeros_like(X)
+            Eij[i, j] = 1
+            temp3 = np.block([[X, Eij], [zeros, X]])
+            gradient[i, j] = np.trace(temp1.T.dot(scipy.linalg.logm(temp2.dot(temp3).dot(temp4))))
 
     return functionValue, gradient
 
-def checkGradient(A, B):
+def checkGradient(X, P, Q, C):
     # numerical gradient checking
     # f(x + t * delta) - f(x - t * delta) / (2t)
     # should be roughly equal to inner product <g, delta>
     t = 1e-6
-    d = A.shape[0]
+    d = X.shape[0]
     delta = np.random.randn(d, d)
-    delta += delta.T
-    f1, _ = fAndG(A + t * delta, B)
-    f2, _ = fAndG(A - t * delta, B)
-    f, g = fAndG(A, B)
+    delta = symm(delta)
+    f1, _ = fAndG(X + t * delta, P, Q, C)
+    f2, _ = fAndG(X - t * delta, P, Q, C)
+    _, g = fAndG(X, P, Q, C)
     print('approximation error',
           np.linalg.norm((f1 - f2) / (2*t) - np.tensordot(g, delta, axes=2)))
 
 def generateRandomData(dim):
-    A = np.random.randn(dim, dim)
-    A = A.T.dot(A)
-    A = 0.5 * (A + A.T) + np.eye(dim)  # make it symmetric and psd
-    B = np.random.randn(dim, dim)
-    B = B.T.dot(B)
-    B = 0.5 * (B + B.T) + np.eye(dim) # make it symmetric and psd
+    def genOneData(dim):
+        A = np.random.randn(dim, dim)
+        A = A.T.dot(A)
+        A = 0.5 * (A + A.T) + np.eye(dim)  # make it symmetric and psd
+        return A
+    
+    X, P, Q, C = genOneData(dim), genOneData(dim), genOneData(dim), genOneData(dim)
 
-    return A, B
+    return X, P, Q, C
 
 if __name__ == '__main__':
-    A, B = generateRandomData(dim=5)
-    functionValue, gradient = fAndG(A, B)
+    X, P, Q, C = generateRandomData(dim=5)
+    functionValue, gradient = fAndG(X, P, Q, C)
     print('functionValue = ', functionValue)
     print('gradient = ', gradient)
 
     print('numerical gradient checking ...')
-    checkGradient(A, B)
+    checkGradient(X, P, Q, C)
